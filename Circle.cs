8214e7ef123e4a3b455f75666761058e5e9cd3df
Madushani <Madushani@DESKTOP-JV13050>
2024-09-02 16:03:21 +0530

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Circle : ShapePrototype
+    {
+        public string color;
+        public int size;
+
+        public Circle(string color,int size) 
+        {
+            this.color = color;
+            this.size = size;
+        }
+
+        public ShapePrototype Clone()
+        {
+            return new Circle(this.color,this.size);
+        }
+        public void Draw()
+        {
+            Console.WriteLine($"Drawing a {color} circle of {size}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Prototype
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Circle circle = new Circle("red", 20);
+            circle.Draw();
+
+            Circle circleCopy = (Circle)circle.Clone();
+            circleCopy.size = 10;
+            circleCopy.Draw();
+        }
+    }
+}
